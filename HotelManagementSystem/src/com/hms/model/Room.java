da23403eadbb454327da33b3bdc0661bcf7996b8
subirpadhee <subir.padhee@colorado.edu>
2016-12-04 04:39:03 -0700

    Room Reservation and Food ordering integrated

--- HotelManagementSystem/src/com/hms/model/Room.java
+++ HotelManagementSystem/src/com/hms/model/Room.java
@@ -1,59 +1,57 @@
 package com.hms.model;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Query;
+import javax.swing.JTable;
+
+import org.hibernate.Session;
+
+import com.hms.model.RoomTable;
 
 public class Room {
-	private int roomNum;
-	private String roomType;
-	private Date roomCheckin;
-	private Date roomCheckout;
-	private String roomStatus;
-	private int numRooms;
-	private double price;
-	
-	public int getRoomNum() {
-		return roomNum;
-	}
-	public void setRoomNum(int roomNum) {
-		this.roomNum = roomNum;
-	}
-	public String getRoomType() {
-		return roomType;
-	}
-	public void setRoomType(String roomType) {
-		this.roomType = roomType;
-	}
-	public Date getRoomCheckin() {
-		return roomCheckin;
-	}
-	public void setRoomCheckin(Date roomCheckin) {
-		this.roomCheckin = roomCheckin;
-	}
-	public Date getRoomCheckout() {
-		return roomCheckout;
-	}
-	public void setRoomCheckout(Date roomCheckout) {
-		this.roomCheckout = roomCheckout;
-	}
-	public String getRoomStatus() {
-		return roomStatus;
-	}
-	public void setRoomStatus(String roomStatus) {
-		this.roomStatus = roomStatus;
-	}
-	public int getNumRooms() {
-		return numRooms;
-	}
-	public void setNumRooms(int numRooms) {
-		this.numRooms = numRooms;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	
-	
 
+	
+	
+	public Room() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void updateRoomTable(Session session, JTable showAvailTable, String checkOutDate, String checkInDate){
+		int numRows = showAvailTable.getRowCount();
+		for(int i =1; i< numRows; i++){
+			String hibernateQuery = "UPDATE RoomTable set roomStatus=:status, roomPrice =:myRoomPrice, wifi =:myWifi, breakfast =:myBreakfast, roomCheckIn=:myroomCheckIn, roomCheckOut=:myroomCheckOut WHERE roomNum =:myRoomNum";
+			Query query = session.createQuery(hibernateQuery);
+			query.setParameter("status", false);
+			query.setParameter("myRoomNum", showAvailTable.getValueAt(i,0));
+			query.setParameter("myBreakfast", showAvailTable.getValueAt(i, 1));
+			query.setParameter("myWifi", showAvailTable.getValueAt(i, 2));
+			query.setParameter("myRoomPrice",Double.parseDouble((String)showAvailTable.getValueAt(i,3)));
+			query.setParameter("myroomCheckIn",checkInDate);
+			query.setParameter("myroomCheckOut",checkOutDate);
+
+			int result = query.executeUpdate();
+			//System.out.println("Rows affected: " + result);	
+		}
+	}
+	
+	
+	public List<RoomTable> getRoomTable(Session session, String roomType){
+		
+		String hibernateQuery = "FROM RoomTable AS E WHERE E.roomType = :Type and E.roomStatus =:Status";
+		Query query 		  = session.createQuery(hibernateQuery);
+		query.setParameter("Type",roomType);
+		query.setParameter("Status",true);
+		
+		List<RoomTable> roomList = ((org.hibernate.Query) query).list();
+		
+		//System.out.println("RoomList:" + roomList.size());
+		return roomList;		
+	}
+	
+	
+	
 }
---
+++ HotelManagementSystem/src/com/hms/view/PaymentView.java
@@ -0,0 +1,193 @@
+package com.hms.view;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JEditorPane;
+import javax.swing.JSpinner;
+import javax.swing.JButton;
+
+public class PaymentView {
+
+	private JFrame frame;
+	private JLabel lblPaymentTo;
+	private JLabel lblPayFor;
+	private JLabel lblCurrency;
+	private JLabel lblAmount;
+	private JLabel lblPaymentDetails;
+	private JLabel lblXyzHotel;
+	private JLabel lblRoomBooking;
+	private JLabel lblNewLabel;
+	private JLabel AmountLabel;
+	private JPanel panel_1;
+	private JLabel lblCardDetails;
+	private JLabel lblCa;
+	private JEditorPane cardNumberPane,nameHolderPane,cvvPane,expiryMonthPane,expiryYearPane;
+	private JLabel lblExpir;
+	JButton btnSubmit;
+	
+	
+
+	public JButton getBtnSubmit() {
+		return btnSubmit;
+	}
+
+	public JEditorPane getCvvPane() {
+		return cvvPane;
+	}
+
+	public JEditorPane getExpiryMonthPane() {
+		return expiryMonthPane;
+	}
+
+	public JEditorPane getExpiryYearPane() {
+		return expiryYearPane;
+	}
+
+	public JEditorPane getCardNumberPane() {
+		return cardNumberPane;
+	}
+
+	public JEditorPane getNameHolderPane() {
+		return nameHolderPane;
+	}
+	
+
+	public JLabel getAmountLabel() {
+		return AmountLabel;
+	}
+
+	public void setAmountLabel(JLabel amountLabel) {
+		AmountLabel = amountLabel;
+	}
+	
+	
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PaymentView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(SystemColor.window);
+		frame.setBackground(SystemColor.window);
+		frame.setBounds(200, 150, 600, 500);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.window);
+		panel.setBounds(12, 5, 576, 150);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		lblPaymentTo = new JLabel("Pay To:");
+		lblPaymentTo.setBounds(193, 47, 70, 15);
+		panel.add(lblPaymentTo);
+		
+		lblPayFor = new JLabel("Pay For:");
+		lblPayFor.setBounds(193, 67, 70, 15);
+		panel.add(lblPayFor);
+		
+		lblCurrency = new JLabel("Currency:");
+		lblCurrency.setBounds(193, 87, 70, 15);
+		panel.add(lblCurrency);
+		
+		lblAmount = new JLabel("Amount:");
+		lblAmount.setBounds(193, 107, 70, 15);
+		panel.add(lblAmount);
+		
+		lblPaymentDetails = new JLabel("Payment Details");
+		lblPaymentDetails.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblPaymentDetails.setBounds(12, 12, 171, 26);
+		panel.add(lblPaymentDetails);
+		
+		lblXyzHotel = new JLabel("XYZ Hotel");
+		lblXyzHotel.setBounds(275, 47, 70, 15);
+		panel.add(lblXyzHotel);
+		
+		lblRoomBooking = new JLabel("Room Booking");
+		lblRoomBooking.setBounds(275, 67, 108, 15);
+		panel.add(lblRoomBooking);
+		
+		lblNewLabel = new JLabel("U.S Dollars");
+		lblNewLabel.setBounds(275, 87, 96, 15);
+		panel.add(lblNewLabel);
+		
+		AmountLabel = new JLabel("SetAmount");
+		AmountLabel.setBounds(275, 107, 70, 15);
+		panel.add(AmountLabel);
+		
+		panel_1 = new JPanel();
+		panel_1.setBackground(SystemColor.window);
+		panel_1.setBounds(12, 166, 576, 322);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		lblCardDetails = new JLabel("Card Details");
+		lblCardDetails.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblCardDetails.setBounds(12, 12, 125, 35);
+		panel_1.add(lblCardDetails);
+		
+		lblCa = new JLabel("Card Number");
+		lblCa.setBounds(131, 56, 110, 21);
+		panel_1.add(lblCa);
+		
+		cardNumberPane = new JEditorPane();
+		cardNumberPane.setBounds(280, 56, 180, 21);
+		panel_1.add(cardNumberPane);
+		
+		lblExpir = new JLabel("Expiry Date (MM)");
+		lblExpir.setBounds(131, 108, 133, 15);
+		panel_1.add(lblExpir);
+		
+		expiryMonthPane = new JEditorPane();
+		expiryMonthPane.setBounds(280, 102, 40, 21);
+		panel_1.add(expiryMonthPane);
+		
+		JLabel label = new JLabel("/");
+		label.setFont(new Font("Dialog", Font.BOLD, 14));
+		label.setBounds(327, 102, 18, 21);
+		panel_1.add(label);
+		
+		expiryYearPane = new JEditorPane();
+		expiryYearPane.setBounds(340, 102, 60, 21);
+		panel_1.add(expiryYearPane);
+		
+		JLabel lblCvvNumber = new JLabel("CVV");
+		lblCvvNumber.setBounds(131, 162, 70, 15);
+		panel_1.add(lblCvvNumber);
+		
+		cvvPane = new JEditorPane();
+		cvvPane.setBounds(280, 156, 40, 21);
+		panel_1.add(cvvPane);
+		
+		JLabel lblNameOnCard = new JLabel("Name on Card");
+		lblNameOnCard.setBounds(131, 216, 125, 21);
+		panel_1.add(lblNameOnCard);
+		
+		btnSubmit = new JButton("Submit");
+		btnSubmit.setBounds(260, 280, 117, 25);
+		panel_1.add(btnSubmit);
+		
+		nameHolderPane = new JEditorPane();
+		nameHolderPane.setBounds(280, 216, 180, 21);
+		panel_1.add(nameHolderPane);
+	}
+
+}
