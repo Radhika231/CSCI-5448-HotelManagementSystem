1ced3c8a52a8d03bf807d6478868595c71b005fc
Meher Jain <meher.jn@gmail.com>
2016-12-05 23:19:11 -0700

    Final Class Diagram Added

--- HotelManagementSystem-Login-SignUp/src/view/UserRole.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-//Factory Method Design Pattern
-//Abstract class which defers instantiation to sub classes
-public abstract class UserRole {
-	public abstract void userScreen();
-
-}
